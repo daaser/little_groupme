04db7811b24c3604f8faeebc05e88d9710fb8861
Doug Aaser <douglasaaser@gmail.com>
2020-03-20 18:03:41 -0400

    Add nickname member variable

--- Models/GroupMe.cs
+++ Models/GroupMe.cs
@@ -4,6 +4,7 @@ namespace groupme.Models {
     public class GroupMe {
         public string text { get; set; }
         public string name { get; set; }
+        public string nickname { get; set; }
         public string user_id { get; set; }
     }
 }
--- gme.sh
+++ gme.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-GROUP_ID=
 curl -s -X GET -H "X-Access-Token: $GM_TOKEN" \
   "https://api.groupme.com/v3/groups/${GROUP_ID}" \
   | jq '.response.members[] | .name + ": " + .user_id'
